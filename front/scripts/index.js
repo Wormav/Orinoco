b563d8b9012be6c2fb4439ae4d9d96cfe729a6b1
jeremy lorette <jeremy.lorette@outlook.com>
2022-03-26 23:23:11 +0100

    page shop with api

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,18 +1,91 @@
 const url = "http://localhost:3000/api/cameras";
 
+const container = document.querySelector("#cardContain");
+
 // appel fetch pour recupéré les données de l'API
 
-async function getAway() {
-  const request = await fetch(url, {
-    method: "GET",
-  });
-  if (!request.ok) {
-    alert("Un problème est survenu.");
-  } else {
-    let data = await request.json();
-    console.log(data);
-  }
+function getItems() {
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .catch((error) => {
+      alert("un problème est survenu.");
+    })
+
+    .then(function (returnAPI) {
+      const items = returnAPI;
+      // console.log(items);
+      for (let item in items) {
+        // container de la carte
+        let itemCardBlock = document.createElement("div");
+        container.appendChild(itemCardBlock);
+        itemCardBlock.classList.add("col-lg-4", "col-md-6", "px-5");
+
+        // carte
+        let itemCard = document.createElement("div");
+        itemCardBlock.appendChild(itemCard);
+        itemCard.classList.add("card", "m-3");
+
+        //img de la carte
+        let itemImg = document.createElement("img");
+        itemCard.appendChild(itemImg);
+        itemImg.classList.add("card-img-top");
+        itemImg.src = returnAPI[item].imageUrl;
+
+        //body de la carte
+        let itemBody = document.createElement("div");
+        itemCard.appendChild(itemBody);
+        itemBody.classList.add("card-body", "bg-primaryP");
+        itemBody.style.minHeight = "130px";
+
+        // titre produit
+        let itemTitle = document.createElement("h5");
+        itemBody.appendChild(itemTitle);
+        itemTitle.classList.add("text-center", "titleShop");
+        itemTitle.style.fontWeight = "bolder";
+        itemTitle.innerText = returnAPI[item].name;
+
+        // description du produit
+        let itemDescription = document.createElement("p");
+        itemBody.appendChild(itemDescription);
+        itemDescription.classList.add("text-center");
+        itemDescription.innerText = returnAPI[item].description;
+
+        // prix du produit
+        let itemPrice = document.createElement("p");
+        itemBody.appendChild(itemPrice);
+        itemPrice.classList.add("text-center");
+        itemPrice.style.fontWeight = "bolder";
+
+        // passage du prix en euro
+        returnAPI[item].price = returnAPI[item].price / 100;
+        itemPrice.innerHTML = new Intl.NumberFormat("fr-FR", {
+          style : "currency",
+          currency : "EUR"
+        }).format(returnAPI[item].price)
+        
+
+        // footer de la carte
+        let itemFooter = document.createElement("div");
+        itemCard.appendChild(itemFooter);
+        itemFooter.classList.add("card-footer", "bg-dark");
+
+        // button achter
+        let cardBtn = document.createElement("button");
+        itemFooter.appendChild(cardBtn);
+        cardBtn.classList.add(
+          "btn",
+          "w-100",
+          "text-secondaryP",
+          "border",
+          "border-secondaryP",
+          "btn-outline-secondary"
+        );
+        cardBtn.innerText = "Acheter";
+        cardBtn.type = "button";
+      }
+    });
 }
 
-getAway();
-
+getItems();
