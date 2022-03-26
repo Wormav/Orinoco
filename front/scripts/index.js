c03ef3325e173ba4733590dfa179a0fc476450db
jeremy lorette <jeremy.lorette@outlook.com>
2022-03-26 21:24:21 +0100

    fetch api

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,18 @@
-// appel fetch pour recupéré les données de l'API 
+const url = "http://localhost:3000/api/cameras";
+
+// appel fetch pour recupéré les données de l'API
+
+async function getAway() {
+  const request = await fetch(url, {
+    method: "GET",
+  });
+  if (!request.ok) {
+    alert("Un problème est survenu.");
+  } else {
+    let data = await request.json();
+    console.log(data);
+  }
+}
+
+getAway();
+
