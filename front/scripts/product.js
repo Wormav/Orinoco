33dafca4732ee0a0133d31532b810ce7a3eb0a10
jeremy lorette <jeremy.lorette@outlook.com>
2022-03-30 16:29:54 +0200

    value quantity

--- front/scripts/product.js
+++ front/scripts/product.js
@@ -1,10 +1,17 @@
 const url = "http://localhost:3000/api/cameras";
 const mainContainer = document.querySelector("main");
 const btn = document.querySelector("#btn");
+let quantity = 1;
+document.querySelector('input#selection').addEventListener('change', (e) => {
+    quantity = e.target.value;
+});
+
+
 // recupere l'id du produit dans l'url
 
 let params = new URLSearchParams(window.location.search);
 let id = params.get("id");
+
 
 //appel API avec ID pour un produit et intégration dans le html
 
@@ -29,6 +36,7 @@ function getItemProduct() {
       name.innerText = returnAPI.name;
 
       //description
+
       let description = document.querySelector("#description");
       description.innerText = returnAPI.description;
 
@@ -43,6 +51,10 @@ function getItemProduct() {
 }
 
 getItemProduct();
+
+
+
+
 
 // click du btn acheter
 
