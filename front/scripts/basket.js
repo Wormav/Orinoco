d5124b7c952ce0b02f25d771cffe2abeba394e6e
jeremy lorette <jeremy.lorette@outlook.com>
2022-04-03 14:52:04 +0200

    basket.js init

--- front/scripts/basket.js
+++ front/scripts/basket.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded" , function () {
-    
-})
+document.addEventListener("DOMContentLoaded", function () {
+  // recupère les produit dans le LS puis les transforme en obj
+
+  const arrayProducts = JSON.parse(localStorage.getItem("products"));
+  const procductsNumber = arrayProducts.length;
+
+  // MAJ nombre d'article dans l'icone panier 
+
+  function numberBasket() {
+    let number = document.getElementById("numberShop");
+    if (procductsNumber === 0)
+     {
+      number.style.display = "none";
+    } else {
+      number.innerText = procductsNumber;
+    }
+  }
+
+
+  
+
+  numberBasket();
+
+  console.log(procductsNumber);
+  console.table(arrayProducts);
+});
